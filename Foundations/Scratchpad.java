3307356fb4fbd3eabaf6b4b0c01b360ac4b3cdfb
Ashraf <ashcoding@outlook.com>
2017-10-02 22:19:23 +0800

    Adding new files.

---
+++ Foundations/Scratchpad.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Scratchpad {
+    public static void main(String[] args) {
+        int[] arr = new int[] {100, 33, 21, 99, 137};
+        System.out.println(Arrays.toString(arr));
+    }
+
+
+}
---
+++ Random/Prime.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.Math;
+
+public class Prime {
+    public static void main(String[] args) {
+        List list = trialDivision(1000);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + ", ");
+        }
+    }
+
+    public static List<Integer> trialDivision(int n) {
+        List<Integer> list = new LinkedList<Integer>();
+        if (n < 2) return list;
+        list.add(2);
+
+        for (int i = 3; i <= n; i++) {
+            // Logic here
+            int check = 1;
+            for (int j = 0; j < list.size(); j++) {
+                check *= i % list.get(j);
+                if (check == 0) break;
+            }
+            if (check != 0) {
+                list.add(i);
+            }
+        }
+        return list;
+    }
+}
